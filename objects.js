cd3a7d3da3d46dfd6f437995426976831962bdbb
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-25 16:04:20 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = {TheStrokes : "Someday"};
 
 function updatePlaylist(playlist, artistName, songTitle){
-Object.assign({}, playlist, { artistName : [songTitle]
+playlist[artistName] = songTitle;
+return playlist;
 })
 }
